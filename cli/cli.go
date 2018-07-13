e8114f0591bf438d4ff94e7c3f01ccf13b5a852f
Romanos Trechlis <r.trechlis@gmail.com>
2018-07-13 12:19:32 +0300

    cli that reads from Stdin

---
+++ cli/cli.go
@@ -0,0 +1,85 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/RomanosTrechlis/go-icls/parse"
+)
+
+// Command holds the actual name of command
+// the several flags that might exist.
+type Command struct {
+	Command string
+	Flags   map[string]string
+}
+
+func (c Command) String() string {
+	s := c.Command + "\n"
+	for k, v := range c.Flags {
+		s += k + "=\"" + v + "\"\n"
+	}
+	return s
+}
+
+// CLI holds the closing channel.
+type CLI struct {
+	closeChan chan struct{}
+}
+
+// New creates a CLI struct.
+func New() *CLI {
+	return &CLI{
+		closeChan: make(chan struct{}, 1),
+	}
+}
+
+// Run begins reading from the Stdin for ever. Parses the
+// command given and apply it to the function passed as parameter.
+//
+// Example:
+//
+// func main() {
+// 	    c := cli.New()
+//	    c.Run(Test)
+// }
+//
+// func Test(cmd cli.Command) {
+//     if cmd.Command == "get" {
+//	       fmt.Fprintf(os.Stdout, "%v", cmd)
+//	   }
+// }
+//
+func (cli *CLI) Run(f func(c Command)) {
+	scanner := bufio.NewScanner(os.Stdin)
+	go func() {
+		fmt.Fprintf(os.Stdout, "> ")
+		for scanner.Scan() {
+			cmd, err := cli.parse(scanner.Text())
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Command '%s' coudn't be parse: %v", err)
+				continue
+			}
+			if cmd.Command == "quit" {
+				cli.quit()
+				return
+			}
+			f(*cmd)
+			fmt.Fprintf(os.Stdout, "> ")
+		}
+	}()
+	<-cli.closeChan
+}
+
+func (cli *CLI) parse(cmd string) (*Command, error) {
+	cName, cFlags, err := parse.Parse(cmd)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse command: %v", err)
+	}
+	return &Command{cName, cFlags}, nil
+}
+
+func (cli *CLI) quit() {
+	close(cli.closeChan)
+}
